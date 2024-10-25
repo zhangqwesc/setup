2e0896941b65cc21e171f9e0c74df0f58f893e43
trickle <trickle@gmail.com>
2024-10-25 10:08:03 +0800

    init commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install -y curl git wget build-essential cmake pkg-config libssl-dev zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+pwd=$(pwd)
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
+cd $pwd
