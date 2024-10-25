a7ee868788667f75074f6c4abd101608c5133506
trickle <trickle@gmail.com>
2024-10-25 10:20:23 +0800

    update

--- setup.sh
+++ setup.sh
@@ -9,3 +9,9 @@ cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-autosuggestions
 cd $pwd
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install eza ripgrep dust bat
+
+echo 'alias ls="eza --icons --time-style iso"' >> ~/.zshrc
+echo 'alias cat="bat -Pp"' >> ~/.zshrc
+echo 'alias du="dust -d1"' >> ~/.zshrc
